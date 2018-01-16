a10e5a402da9ee6c4462e395b97ec515e3ade02a
TopherYoungGBX <cryoung488@gmail.com>
2018-01-15 18:04:34 -0800

    Content section build out, responsive styling

--- themes/topher/source/js/script.js
+++ themes/topher/source/js/script.js
@@ -42,3 +42,30 @@
   });
 
 })(jQuery);
+
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 800,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'loading-icon',
+    loadingInner: '<img src="../images/topher_loader.gif" />', // e.g '<img src="loading.svg" />'
+    timeout: true,
+    timeoutCountdown: 3000,
+    onLoadEvent: true,
+    browser: ['animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay: false,
+    overlayClass: 'animsition-overlay-slide',
+    overlayParentElement: 'body',
+  })
+    .one('animsition.inEnd', function() {
+      $('body').addClass('loaded');
+    });
+});
